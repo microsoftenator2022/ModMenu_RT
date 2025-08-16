9f15608cd3cdcd3d9e3c8aed0a58ef678b5c8093
Aaron Maslen <aaron.maslen@gmail.com>
2025-08-16 16:27:42 +1000

    Fix "Enumeration changed" exception in OwlcatToggleGroup

---
+++ ModMenu/FixOwlcatToggleGroup.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using Owlcat.Runtime.UI.Controls.Toggles;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+
+namespace ModMenu
+{
+  [HarmonyPatch(typeof(OwlcatToggleGroup), nameof(OwlcatToggleGroup.HandleToggleOn))]
+  internal static class FixOwlcatToggleGroup
+  {
+    static Dictionary<OwlcatToggle, IDisposable> MakeCopy(Dictionary<OwlcatToggle, IDisposable> original)
+    {
+      var copy = new Dictionary<OwlcatToggle, IDisposable>();
+      foreach (var entry in original)
+        copy.Add(entry.Key, entry.Value);
+
+      return copy;
+    }
+
+    static readonly MethodInfo get_Keys =
+      typeof(Dictionary<OwlcatToggle, IDisposable>)
+        .GetProperty(nameof(Dictionary<,>.Keys))
+        .GetMethod;
+
+    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+      foreach (var instruction in instructions)
+      {
+        if (instruction.Calls(get_Keys))
+          yield return CodeInstruction.Call((Dictionary<OwlcatToggle, IDisposable> original) => MakeCopy(original));
+
+        yield return instruction;
+      }
+    }
+  }
+}
